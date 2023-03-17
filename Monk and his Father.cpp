970f6167acaccfd7a619878c87f2ed5801045958
RafayethRafi <87166899+RafayethRafi@users.noreply.github.com>
2023-03-17 21:03:33 +0600

    Create Monk and his Father.cpp
    https://www.hackerearth.com/problem/algorithm/monk-and-his-father-93b639f4/

---
+++ Monk and his Father.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+int main()
+{
+    optimize();
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long x;
+        cin>>x;
+
+        cout<<__builtin_popcountll(x)<<endl;
+    }
+    return 0;
+}
